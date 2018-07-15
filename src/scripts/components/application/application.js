5c9b6aaf360b142d395738d55ca8427b0ad63c7b
Rafael Camargo <contato@rafaelcamargo.com>
2018-07-14 23:51:31 -0300

    Created application component

---
+++ src/scripts/components/application/application.js
@@ -0,0 +1,53 @@
+import '@styles/application.styl';
+import domService from '../../services/dom/dom';
+import textService from '../../services/text/text';
+import template from './application.html';
+
+export class Application {
+  constructor(applicationType, options = {}){
+    this.type = applicationType;
+    this.options = options;
+    this.element = buildElement(applicationType);
+    this.setOptions(this.options);
+  }
+  setOptions(options){
+    if(options.minHeight)
+      this.setMinHeight(options.minHeight);
+    if(options.windowTitle)
+      this.setWindowTitle(options.windowTitle);
+  }
+  setMinHeight(height){
+    const container = getContentContainerElement(this.element);
+    container.style.minHeight = height;
+  }
+  setWindowTitle(title){
+    const titleContainerElement = getWindowTitleContainerElement(this.element);
+    titleContainerElement.innerText = title;
+    this.windowTitle = title;
+  }
+  addContent(content){
+    const container = getContentContainerElement(this.element);
+    container.appendChild(content);
+  }
+}
+
+function buildElement(applicationType, windowTitle){
+  let element = buildWrapper(applicationType);
+  element.appendChild(domService.parseHtml(template));
+  return element;
+}
+
+function buildWrapper(applicationType){
+  const wrapper = document.createElement('div');
+  const cssClass = `${textService.toKebabCase(applicationType)}-application`;
+  wrapper.setAttribute('class', cssClass);
+  return wrapper;
+}
+
+function getContentContainerElement(applicationElement){
+  return applicationElement.querySelector('[data-content-container]');
+}
+
+function getWindowTitleContainerElement(applicationElement){
+  return applicationElement.querySelector('[data-title-container]');
+}
---
+++ src/scripts/components/application/application.test.js
@@ -0,0 +1,51 @@
+import { Application } from './application';
+
+describe('Application Component', () => {
+
+  it('should instantiate application from application type', () => {
+    const application = new Application('editor');
+    expect(application.element.classList.length).toEqual(1);
+    expect(application.element.classList[0]).toEqual('editor-application');
+  });
+
+  it('should have a topbar', () => {
+    const application = new Application('editor');
+    const topbar = application.element.querySelector('application-topbar');
+    expect(topbar).toBeDefined();
+  });
+
+  it('should have a title container', () => {
+    const application = new Application('editor');
+    const titleContainer = application.element.querySelector('[data-title-container]');
+    expect(titleContainer).toBeDefined();
+  });
+
+  it('should have a content container', () => {
+    const application = new Application('editor');
+    const contentContainer = application.element.querySelector('[data-content-container]');
+    expect(contentContainer).toBeDefined();
+  });
+
+  it('should allow minimum height option', () => {
+    const minHeight = '300px';
+    const application = new Application('editor', { minHeight });
+    const contentContainer = application.element.querySelector('[data-content-container]');
+    expect(contentContainer.style.minHeight).toEqual(minHeight);
+  });
+
+  it('should allow window title option', () => {
+    const windowTitle = 'Atom';
+    const application = new Application('editor', { windowTitle });
+    const titleContainer = application.element.querySelector('[data-title-container]');
+    expect(titleContainer.innerText).toEqual(windowTitle);
+  });
+
+  it('should add content', () => {
+    const content = document.createElement('h1');
+    const application = new Application('editor');
+    content.innerText = 'Some content.';
+    application.addContent(content);
+    expect(application.element.querySelector('h1')).toEqual(content);
+  });
+
+});
