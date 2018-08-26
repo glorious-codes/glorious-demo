26c968d39898169a0b7f5ad066e8effad2da55b9
Rafael Camargo <contato@rafaelcamargo.com>
2018-08-26 17:38:18 -0300

    Making type service apply typing css class to container element on type

--- src/scripts/services/type/type.js
+++ src/scripts/services/type/type.js
@@ -4,11 +4,24 @@ import typeHtmlTextService from '../type-html-text/type-html-text';
 
 const _public = {};
 
+const TYPING_CSS_CLASS = 'is-typing';
+
 _public.type = (container, text, onComplete) => {
-  if(textService.containsHtml(text))
-    typeHtmlTextService.type(container, text, onComplete);
-  else
-    typePlainTextService.type(container, text, onComplete);
+  handleIsTypingCssClass(container, 'add', TYPING_CSS_CLASS);
+  getSpecificTypeService(text).type(container, text, () => {
+    handleIsTypingCssClass(container, 'remove', TYPING_CSS_CLASS);
+    onComplete();
+  })
 };
+
+function handleIsTypingCssClass(element, action, cssClass){
+  element.classList[action](cssClass);
+}
+
+function getSpecificTypeService(text){
+  if(textService.containsHtml(text))
+    return typeHtmlTextService;
+  return typePlainTextService;
+}
 
 export default _public;
