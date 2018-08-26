2a7edc5cfd5437a087cecef0903795e2dc5ea77e
Rafael Camargo <contato@rafaelcamargo.com>
2018-08-26 17:37:24 -0300

    Minor improvement on type plain text service

--- src/scripts/services/type-plain-text/type-plain-text.js
+++ src/scripts/services/type-plain-text/type-plain-text.js
@@ -3,14 +3,17 @@ const _public = {};
 _public.type = (container, text, onComplete) => {
   const letters = text.split('');
   const letter = letters.shift();
-  if(letter) {
-    container.append(letter);
-    setTimeout(() => {
-      _public.type(container, letters.join(''), onComplete);
-    }, 75);
-  } else {
+  if(letter)
+    appendLetter(container, letter, letters, onComplete);
+  else
     onComplete();
-  }
+}
+
+function appendLetter(container, letter, letters, onComplete){
+  container.append(letter);
+  setTimeout(() => {
+    _public.type(container, letters.join(''), onComplete);
+  }, 75);
 }
 
 export default _public;
