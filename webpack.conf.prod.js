c17f92ece544d96a7cc9cf233dfc8eb21beaac09
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-25 13:57:10 -0300

    0.8.2

--- webpack.conf.prod.js
+++ webpack.conf.prod.js
@@ -6,7 +6,6 @@ const project = require('./project.json');
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
   output: {
     filename: project.scripts.dist.filename.prod
   },
@@ -16,16 +15,14 @@ module.exports = {
         terserOptions: {
           mangle: true,
           compress: {
-            warnings: false,
-          },
-          sourceMap: true
+            warnings: false
+          }
         }
       }),
       new OptimizeCSSAssetsPlugin()
     ]
   },
   plugins: [
-    new webpack.SourceMapDevToolPlugin(),
     new MiniCssExtractPlugin({
       filename: project.styles.dist.filename.prod
     })
