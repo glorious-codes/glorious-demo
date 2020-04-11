14e8f5685a41c82d508e0c2a7b1cdcb93ec2d193
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-11 20:34:58 -0300

    chore: minor improvements on desktop class

--- src/scripts/components/desktop/desktop.js
+++ src/scripts/components/desktop/desktop.js
@@ -4,8 +4,6 @@ import { TerminalApplication } from '../terminal-application/terminal-applicatio
 import domService from '../../services/dom/dom';
 import template from './desktop.html';
 
-const ANIMATION_DEFAULT_DURATION = 750;
-
 export class Desktop {
   constructor(container){
     this.element = domService.parseHtml(template);
@@ -13,35 +11,34 @@ export class Desktop {
     this.openApplications = [];
     this.container.appendChild(this.element);
   }
-  openApplication(applicationType, applicationOptions){
-    const application = getOpenApplication(this.openApplications, applicationType);
-    if(!application)
-      return buildApplication(this, applicationType, applicationOptions);
-    return application;
+  openApplication(appType, appOptions){
+    const app = getOpenApplication(this.openApplications, appType);
+    return app ? app : buildApplication(this, appType, appOptions);
   }
   minimizeAllApplications(onComplete){
-    for(let i = 0; i < this.openApplications.length; i++)
-      this.openApplications[i].minimize();
-    setTimeout(onComplete, ANIMATION_DEFAULT_DURATION);
+    this.openApplications.forEach(openApplication => openApplication.minimize());
+    setTimeout(onComplete, getDefaultAnimationDuration());
   }
   maximizeApplication(application, onComplete){
     application.maximize();
-    setTimeout(onComplete, ANIMATION_DEFAULT_DURATION);
+    setTimeout(onComplete, getDefaultAnimationDuration());
   }
 }
 
-function buildApplication(desktop, applicationType, options){
-  const Application = applicationType == 'editor' ? EditorApplication: TerminalApplication;
+function buildApplication(desktop, appType, options){
+  const Application = appType == 'editor' ? EditorApplication: TerminalApplication;
   const application = new Application(desktop.element, options);
   desktop.openApplications.push(application);
   desktop.element.appendChild(application.element);
   return application;
 }
 
-function getOpenApplication(openApplications, applicationType){
-  for(let i = 0; i < openApplications.length; i++){
-    const application = openApplications[i];
-    if(application.type == applicationType)
-      return application;
-  }
+function getOpenApplication(openApplications, appType){
+  return openApplications.find(openApplication => {
+    return openApplication.type === appType;
+  });
+}
+
+function getDefaultAnimationDuration(){
+  return 750;
 }
